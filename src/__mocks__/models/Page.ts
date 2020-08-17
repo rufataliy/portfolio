6776e41d8a3c55646c875cf6e594004b741418f2
Rufat <rufataliyevbakou@gmail.com>
2020-08-17 18:01:27 -0400

    Added page and pages mocks model

---
+++ src/__mocks__/models/Page.ts
@@ -0,0 +1,6 @@
+export interface Page {
+  title: string;
+  path: string;
+  content: string;
+  thumbnail: string;
+}
---
+++ src/__mocks__/pages.ts
@@ -0,0 +1,10 @@
+import { Page } from "./models";
+
+export const pages: Page[] = [
+  {
+    title: "Page",
+    content: "<p>lorem lorem30",
+    path: "link",
+    thumbnail: "linkimg",
+  },
+];
