3d80c93ca2e6c1b926858a6320de6ec257409c2c
Rufat <rufataliyevbakou@gmail.com>
2020-08-17 18:00:28 -0400

    Added Pages and theme logic to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { Pages } from "./components";
+import { ThemeProvider } from "styled-components";
+import { DarkTheme, LightTheme, GlobalStyles } from "./theme";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.bg};
+  color: ${({ theme }) => theme.colors.fontColors.medium};
+  height: 100%;
+  width: 100%;
+`;
 
 function App() {
-  return <div className="App"></div>;
+  const [lightMode, setLightMode] = useState(true);
+  const getTheme = (lightMode: boolean) => {
+    if (lightMode) {
+      return LightTheme;
+    }
+    return DarkTheme;
+  };
+  return (
+    <ThemeProvider theme={getTheme(lightMode)}>
+      <Wrapper>
+        <button onClick={() => setLightMode(!lightMode)}>togggle theme</button>
+        <GlobalStyles />
+        <Pages />
+      </Wrapper>
+    </ThemeProvider>
+  );
 }
 
 export default App;
