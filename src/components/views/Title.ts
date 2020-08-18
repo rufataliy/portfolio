cc51eca94f21d78cacaf631286476d56b4c2880e
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-18 16:12:12 -0400

    Added Title to views

---
+++ src/components/views/Title.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Title = styled.div`
+  z-index: 10;
+  position: absolute;
+  top: 0;
+  left: 5%;
+  padding: 1rem 1.4rem;
+  background: white;
+  box-shadow: 0px 9px 10px -6px rgba(0, 0, 0, 0.7);
+  background-color: ${({ theme }) => theme.colors.bg};
+  color: ${({ theme }) => theme.colors.fontColors.medium};
+  border-radius: 0.5rem;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  font-size: 1.8rem;
+  margin: 0;
+`;
