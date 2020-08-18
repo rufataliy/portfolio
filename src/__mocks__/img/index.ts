3b84feac5f1d1f3289722aa69bd745424773fa0b
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-18 15:45:29 -0400

    Added some img for mock data

---
+++ src/__mocks__/img/index.ts
@@ -0,0 +1,7 @@
+export { default as profile } from "./profile.png";
+export { default as project } from "./project.svg";
+export { default as contact } from "./contact.svg";
+export { default as blog } from "./blog.svg";
+export { default as zengarden } from "./zengarden.png";
+export { default as sanntek } from "./sanntek.png";
+export { default as prodigy } from "./prodigy.png";
