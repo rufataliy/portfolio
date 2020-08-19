c1f010b8a3726f829954f288adada0edf170b753
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-18 22:56:38 -0400

    Added animation to the Box element

--- src/components/views/Box.tsx
+++ src/components/views/Box.tsx
@@ -8,8 +8,6 @@ export const Box = styled.div`
   padding: 2vw;
   height: 250px;
   width: 500px;
-  box-shadow: ${({ theme }) =>
-    `${theme.colors.shadow.value}  ${theme.colors.shadow.color}`};
   transition: 0.2s linear;
   border-radius: 10px;
   cursor: pointer;
@@ -18,9 +16,9 @@ export const Box = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: flex-start;
-  box-shadow: 0px 0px 10px 0px white;
+  box-shadow: 0px 2px 17px -6px ${({ theme }) => theme.colors.shadow.color};
   animation: shadowPop 0.5s 1 ease-out forwards;
-
+  animation-delay: 0.1s;
   .page-card-img {
     height: 100%;
     position: absolute;
@@ -33,17 +31,18 @@ export const Box = styled.div`
     border-radius: 5px;
   }
 
-  &:hover {
-    transform: scale(1.05);
-    box-shadow: 0px 9px 25px -15px ${({ theme }) => theme.colors.shadow.hover};
-  }
-
   @keyframes shadowPop {
     0% {
-      box-shadow: 0px 0px 10px 0px white;
+      box-shadow: 0px 2px 17px -6px ${({ theme }) => theme.colors.shadow.color};
     }
     100% {
-      box-shadow: 0px 9px 25px -15px ${({ theme }) => theme.colors.shadow.hover};
+      box-shadow: ${({ theme }) =>
+        `${theme.colors.shadow.value} ${theme.colors.shadow.color}`};
     }
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0px -9px 25px -15px ${({ theme }) => theme.colors.shadow.hover};
   }
 `;
