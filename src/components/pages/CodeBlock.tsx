10e828fb52bb9b6418c0e2c1c9e4d6a167dd3fae
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-21 00:42:14 -0400

    Created CodeBlock for ReactMarkdown renderer for code examples

---
+++ src/components/pages/CodeBlock.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Highlighter from "react-syntax-highlighter";
+import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+
+interface Props {
+  language: string;
+  value: string;
+}
+
+export const CodeBlock: React.FC<Props> = ({ language, value }) => {
+  console.log(language);
+
+  return (
+    <Highlighter style={atomOneDark} language={language}>
+      {value}
+    </Highlighter>
+  );
+};
