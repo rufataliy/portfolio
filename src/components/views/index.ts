9adf3e90d1af0d647301b7d8d3f9fe40a2cb0378
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-22 12:29:25 -0400

    Deleted BoxOpen file

--- src/components/views/index.ts
+++ src/components/views/index.ts
@@ -1,6 +1,5 @@
 export { Box } from "./Box";
 export { BoxWrapper } from "./BoxWrapper";
-export { BoxOpen } from "./BoxOpen";
 export { Title } from "./Title";
 export { Content } from "./Content";
 export { Toggle } from "./Toggle";
