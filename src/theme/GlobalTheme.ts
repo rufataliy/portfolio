8140c41afaa68edd8fee101227fda2a2877b9a12
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-22 12:22:03 -0400

    Added accents colors in to the global theme

--- src/theme/GlobalTheme.ts
+++ src/theme/GlobalTheme.ts
@@ -5,6 +5,7 @@ const fontColorsDark = {
   medium: "#ccced1",
   low: "#e7e8e9",
 };
+
 const fontColorsLight = {
   high: "#3f4349",
   medium: "#5f6368",
@@ -16,21 +17,29 @@ const shadowDark = {
   value: "0px 9px 17px -6px",
   hover: "rgba(0, 0, 0, 0.9)",
 };
+
 const shadowLight = {
   color: "rgba(0, 0, 0, 0.7)",
   value: "0px 9px 17px -6px",
   hover: "rgba(0, 0, 0, 0.9)",
 };
 
+const accents = {
+  primary: "#fdb415",
+};
+
 const bgDark = "#35363a";
 const bgLight = "#ffffff";
 
 const darkColors = {
+  accents,
   fontColors: fontColorsDark,
   shadow: shadowDark,
   bg: bgDark,
 };
+
 const lightColors = {
+  accents,
   fontColors: fontColorsLight,
   shadow: shadowLight,
   bg: bgLight,
@@ -39,6 +48,7 @@ const lightColors = {
 export const LightTheme: DefaultTheme = {
   colors: lightColors,
 };
+
 export const DarkTheme: DefaultTheme = {
   colors: darkColors,
 };
--- src/theme/models/Theme.ts
+++ src/theme/models/Theme.ts
@@ -1,11 +1,13 @@
 import { FontColors } from "./FontColors";
 import { Bg } from "./Bg";
 import { Shadow } from "./Shadow";
+import { Accents } from "./Accents";
 
 export interface Theme {
   colors: {
     fontColors: FontColors;
     bg: Bg;
     shadow: Shadow;
+    accents: Accents;
   };
 }
