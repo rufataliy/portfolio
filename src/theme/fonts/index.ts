5a8c5e3c3fb03ebe57b6e6add05db399035b4b20
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-22 15:20:17 -0400

    Added font-face

---
+++ src/theme/fonts/index.ts
@@ -0,0 +1,6 @@
+export { default as baloobhaina2_bold_woff } from "./baloobhaina2-bold-webfont.woff";
+export { default as baloobhaina2_bold_woff2 } from "./baloobhaina2-bold-webfont.woff2";
+export { default as baloobhaina2_regular_woff } from "./baloobhaina2-regular-webfont.woff";
+export { default as baloobhaina2_regular_woff2 } from "./baloobhaina2-regular-webfont.woff2";
+export { default as baloobhaina2_semibold_woff } from "./baloobhaina2-semibold-webfont.woff";
+export { default as baloobhaina2_semibold_woff2 } from "./baloobhaina2-semibold-webfont.woff2";
