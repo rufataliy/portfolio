7495d7ddc5c65891a7711482ca3b105e3e8ac4c3
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-24 13:14:52 -0400

    Added font icond to the fonts

--- src/theme/fonts/index.ts
+++ src/theme/fonts/index.ts
@@ -4,3 +4,5 @@ export { default as baloobhaina2_regular_woff } from "./baloobhaina2-regular-web
 export { default as baloobhaina2_regular_woff2 } from "./baloobhaina2-regular-webfont.woff2";
 export { default as baloobhaina2_semibold_woff } from "./baloobhaina2-semibold-webfont.woff";
 export { default as baloobhaina2_semibold_woff2 } from "./baloobhaina2-semibold-webfont.woff2";
+export { default as iconfont_woff } from "./icofont.woff";
+export { default as iconfont_woff2 } from "./icofont.woff2";
