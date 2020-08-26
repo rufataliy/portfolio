8dd508550eca5f4bd281b8a176c31c7df50ff9c4
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-25 23:00:11 -0400

    Modified header to be responsive and changed toggle component

--- src/components/views/Icon.tsx
+++ src/components/views/Icon.tsx
@@ -2,10 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 const Span = styled.span`
-  height: 22px;
-  margin: 0 10px;
-  display: inline-block;
-  font-size: 1.1rem;
+  display: flex;
 `;
 
 interface Props {
