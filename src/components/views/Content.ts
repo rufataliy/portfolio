3e83687ca7054a061015467845960c163382abd1
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-08-30 15:10:43 -0400

    Modified styles for Content

--- src/components/views/Content.ts
+++ src/components/views/Content.ts
@@ -7,6 +7,20 @@ export const Content = styled.div`
   position: relative;
   padding-bottom: 70px;
   min-height: 100%;
+  & > * {
+    margin-bottom: 1.2rem;
+  }
+  & p > code {
+    padding: 0 7px;
+    padding-top: 4px;
+    border-radius: 5px;
+    vertical-align: middle;
+    font-size: 1.1rem;
+    line-height: 1;
+    display: inline-flex;
+    background: #ececec;
+    color: #35363a;
+  }
   a {
     transition: 0.3s ease-out;
     text-decoration: underline;
@@ -17,9 +31,7 @@ export const Content = styled.div`
   p {
     line-height: 1.5;
     font-size: 1.1rem;
-    margin-bottom: 1.2rem;
   }
-
   img {
     width: 100%;
     border-radius: 5px;
