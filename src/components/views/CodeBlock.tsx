6924eb6d1b1b7222822405e23a830fc7956d8e5f
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-01 23:58:59 -0400

    CodeBlock import made partial and bg changed

--- src/components/views/CodeBlock.tsx
+++ src/components/views/CodeBlock.tsx
@@ -1,6 +1,20 @@
 import React from "react";
-import Highlighter from "react-syntax-highlighter";
-import { atomOneDark } from "react-syntax-highlighter/dist/esm/styles/hljs";
+import { PrismLight as Highlighter } from "react-syntax-highlighter";
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+import styled from "styled-components";
+
+Highlighter.registerLanguage("tsx", tsx);
+Highlighter.registerLanguage("css", css);
+Highlighter.registerLanguage("javascript", js);
+
+const Wrapper = styled.div`
+  & pre {
+    background: #001e31 !important;
+  }
+`;
 
 interface Props {
   language: string;
@@ -9,8 +23,10 @@ interface Props {
 
 export const CodeBlock: React.FC<Props> = ({ language, value }) => {
   return (
-    <Highlighter style={atomOneDark} language={language}>
-      {value}
-    </Highlighter>
+    <Wrapper>
+      <Highlighter style={tomorrow} language={language}>
+        {value}
+      </Highlighter>
+    </Wrapper>
   );
 };
