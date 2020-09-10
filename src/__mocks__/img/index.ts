1076cec81f3eac59a581058a3c1a4307f8f3b7c5
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-09-10 11:54:16 -0400

    Made soem changes to mock images

--- src/__mocks__/img/index.ts
+++ src/__mocks__/img/index.ts
@@ -1,7 +1,7 @@
-export { default as profile } from "./profile.webp";
+export { default as profile } from "./profile.svg";
 export { default as project } from "./project.svg";
 export { default as contact } from "./contact.svg";
 export { default as blog } from "./blog.svg";
-export { default as zengarden } from "./zengarden.webp";
-export { default as sanntek } from "./sanntek.webp";
+export { default as zengarden } from "./zengarden.jpg";
+export { default as sanntek } from "./sanntek.jpg";
 export { default as prodigy } from "./prodigy.png";
